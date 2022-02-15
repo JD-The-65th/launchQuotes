a78e226ae33aea4c8bafa22d6c587ad533f3d75d
JD Williams <jdwill239@gmail.com>
2022-02-15 15:36:57 -0500

    another namespace issue?

--- include/ModConfig.hpp
+++ include/ModConfig.hpp
@@ -4,12 +4,12 @@
 #include "config-utils/shared/config-utils.hpp"
 
 DECLARE_CONFIG(ModConfig,
-    CONFIG_VALUE(quote1, string, "quote1", "i forgor");
-    CONFIG_VALUE(quote2, string, "quote2", "lmao");
-    CONFIG_VALUE(quote3, string, "quote3", "e");
-    CONFIG_VALUE(quote4, string, "quote4", "mr strimer play rap gode");
-    CONFIG_VALUE(quote5, string, "quote5", "i forgor");
-    CONFIG_VALUE(quote6, string, "quote6", "lmao");
+    CONFIG_VALUE(quote1, std::string, "quote1", "i forgor");
+    CONFIG_VALUE(quote2, std::string, "quote2", "lmao");
+    CONFIG_VALUE(quote3, std::string, "quote3", "e");
+    CONFIG_VALUE(quote4, std::string, "quote4", "mr strimer play rap gode");
+    CONFIG_VALUE(quote5, std::string, "quote5", "i forgor");
+    CONFIG_VALUE(quote6, std::string, "quote6", "lmao");
 
     CONFIG_INIT_FUNCTION(
         CONFIG_INIT_VALUE(quote1);
