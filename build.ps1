52aebf8eb9828f82f465804830cd095873849897
JD-The-65th <jdwill239@gmail.com>
2022-04-17 19:25:04 -0400

    Update build stuff

--- build.ps1
+++ build.ps1
@@ -1,7 +1,19 @@
 Param(
     [Parameter(Mandatory=$false)]
-    [Switch]$clean
+    [Switch] $clean,
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $help
 )
+
+if ($help -eq $true) {
+    echo "`"Build`" - Copiles your mod into a `".so`" or a `".a`" library"
+    echo "`n-- Arguments --`n"
+
+    echo "-Clean `t`t Deletes the `"build`" folder, so that the entire library is rebuilt"
+
+    exit
+}
 
 # if user specified clean, remove all build files
 if ($clean.IsPresent)
@@ -12,14 +24,13 @@ if ($clean.IsPresent)
     }
 }
 
-$NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
 
 if (($clean.IsPresent) -or (-not (Test-Path -Path "build")))
 {
     $out = new-item -Path build -ItemType Directory
-}
+} 
 
 cd build
-& cmake -G "Ninja" -DCMAKE_BUILD_TYPE="Release" ../
-& cmake --build . -j 8
+& cmake -G "Ninja" -DCMAKE_BUILD_TYPE="RelWithDebInfo" ../
+& cmake --build .
 cd ..
